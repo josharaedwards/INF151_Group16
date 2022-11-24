7acd231ccba4ff2df9f8a49795317ae9988ee16b
josharaedwards <josharaedwards@gmail.com>
2022-11-23 16:43:58 -0800

    First Major Update
    * Able to connect to Photon
    * Created a main menu and lobby
    * Created multiple rooms depending on the size of the party

---
+++ Web Escape/Assets/Scripts/Launcher.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    [SerializeField]
+    private GameObject controlPanel;
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    [SerializeField]
+    private GameObject progressLabel;
+
+    private bool isConnecting;
+
+    string gameVersion = "1";
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+    }
+
+    public void Connect()
+    {
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+
+        if(PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+
+        if(isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        isConnecting = false;
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the 'Room for 1'");
+            PhotonNetwork.LoadLevel("Room for 1");
+        }
+    }
+}
---
+++ Web Escape/Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class PlayerNameInputField : MonoBehaviour
+{
+    const string playerNamePrefKey = "PlayerName";
+
+    void Start()
+    {
+        string defaultName = string.Empty;
+        TMP_InputField _inputField = this.GetComponent<TMP_InputField>();
+
+        if(_inputField != null)
+        {
+            if(PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string value)
+    {
+        if(string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+
+        PhotonNetwork.NickName = value;
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
